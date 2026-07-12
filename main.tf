80d08be52168745771e3523bdf5c7c97af9c07ce
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:58:01 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
+data "azurerm_key_vault_secret" "certificate_password" {
+  for_each     = { for k, v in var.container_app_environment_custom_domains : k => v if v.certificate_password_key_vault_id != null && v.certificate_password_key_vault_secret_name != null }
+  name         = each.value.certificate_password_key_vault_secret_name
+  key_vault_id = each.value.certificate_password_key_vault_id
+}
 resource "azurerm_container_app_environment_custom_domain" "container_app_environment_custom_domains" {
   for_each = var.container_app_environment_custom_domains
 
   certificate_blob_base64      = each.value.certificate_blob_base64
-  certificate_password         = each.value.certificate_password
+  certificate_password         = each.value.certificate_password != null ? each.value.certificate_password : try(data.azurerm_key_vault_secret.certificate_password[each.key].value, null)
   container_app_environment_id = each.value.container_app_environment_id
   dns_suffix                   = each.value.dns_suffix
 }
--- variables.tf
+++ variables.tf
@@ -4,15 +4,19 @@ Map of container_app_environment_custom_domains, attributes below
 Required:
     - certificate_blob_base64
     - certificate_password
+    - certificate_password_key_vault_id (alternative to certificate_password - read from Key Vault instead)
+    - certificate_password_key_vault_secret_name (alternative to certificate_password - read from Key Vault instead)
     - container_app_environment_id
     - dns_suffix
 EOT
 
   type = map(object({
-    certificate_blob_base64      = string
-    certificate_password         = string
-    container_app_environment_id = string
-    dns_suffix                   = string
+    certificate_blob_base64                    = string
+    certificate_password                       = string
+    certificate_password_key_vault_id          = optional(string)
+    certificate_password_key_vault_secret_name = optional(string)
+    container_app_environment_id               = string
+    dns_suffix                                 = string
   }))
 }
 
