1e6cecd819a62dd7f567288cda5775ea90db6bd0
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:31:57 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@ Map of container_app_environment_custom_domains, attributes below
 Required:
     - certificate_blob_base64
     - certificate_password
-    - certificate_password_key_vault_id (alternative to certificate_password - read from Key Vault instead)
-    - certificate_password_key_vault_secret_name (alternative to certificate_password - read from Key Vault instead)
+    - certificate_password_key_vault_id (optional, alternative to certificate_password)
+    - certificate_password_key_vault_secret_name (optional, alternative to certificate_password)
     - container_app_environment_id
     - dns_suffix
 EOT
